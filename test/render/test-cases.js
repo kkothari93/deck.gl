93e4ca419ec65275e5c5f4c580574537608ff7a1
Jian Huang <jianh@uber.com>
2018-06-25 13:35:32 -0700

    fix rendering test image names

--- test/render/test-cases.js
+++ test/render/test-cases.js
@@ -1049,7 +1049,7 @@ export const TEST_CASES = [
         gpuAggregation: true
       })
     ],
-    referenceImageUrl: './test/render/golden-images/grid-lnglat.png'
+    referenceImageUrl: './test/render/golden-images/gpu-grid-lnglat.png'
   },
   {
     name: 'contour-lnglat',
@@ -1076,6 +1076,6 @@ export const TEST_CASES = [
         ]
       })
     ],
-    referenceImageUrl: './test/render/golden-images/grid-lnglat.png'
+    referenceImageUrl: './test/render/golden-images/contour-lnglat.png'
   }
 ];
